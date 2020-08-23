b8292f5b5e4c51e0f1760b72e00104b42a2dd3b0
numeroflip <berenyi.aron@hotmail.hu>
2020-08-23 16:23:51 +0200

    Adding cryptocompare

--- src/components/App/AppProvider.js
+++ src/components/App/AppProvider.js
@@ -1,17 +1,31 @@
-import React, {useState } from 'react'
-
+import React, {useState, useEffect} from 'react'
+// import cc from 'cryptocompare'
+const cc = require('cryptocompare')
+cc.setApiKey('6bce00cebd36b06c07f20e0e94c2a0fe1b0211dc0a8a8dc030f6fb8b4117f707')
 export const AppContext = React.createContext();
 
 export default ({children}) => {
+    
     const [page, setPage] = useState('dashboard');
     const [favourites, setFavourites] = useState('');
     const [firstVisit, setFirstVisit] = useState(localStorage.getItem('ancrypto' ? false : true))
-
-    const savedSettings = () => {
-        let appData = JSON.parse(localStorage.getItem('ancrypto'));
-        if(appData) { setFirstVisit(false)}
-
+    const [coinList, setCoinList] = useState([])
+    
+    
+    const fetchCoins = async () => {
+        let coins = await cc.coinList()
+        coins = coins.Data
+        try {return coins}
+        catch(e) {console.log(e)}
     }
+
+    useEffect( () => {
+        async function getCoins() {
+            let coins = await fetchCoins()
+            setCoinList(coins)
+            console.log(coinList)
+        }
+        getCoins()}, [])
 
     const confirmFavourites = () => {
         console.log('I am a genius')
@@ -21,6 +35,6 @@ export default ({children}) => {
     }
 
     return (
-    <AppContext.Provider value={{page, setPage, favourites, confirmFavourites, firstVisit}}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{page, coinList, setPage, favourites, confirmFavourites, firstVisit}}>{children}</AppContext.Provider>
     )
 }
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,17 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import AppLayout from './AppLayout'
 import AppBar from './AppBar'
-import { AppContext } from './AppProvider'
+import AppProvider from './AppProvider'
 import Settings from '../Settings'
 
 export default function App () {
 
-
   return (
     <AppLayout>
+      <AppProvider>
         <AppBar />
         <Settings />
+      </AppProvider>
     </AppLayout>
 
   );
