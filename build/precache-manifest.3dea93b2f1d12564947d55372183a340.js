22e542ec2d283da78eb66a7b2245b97004241411
numeroflip <berenyi.aron@hotmail.hu>
2020-08-30 10:02:55 +0200

    generating build files

---
+++ build/precache-manifest.3dea93b2f1d12564947d55372183a340.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96a8430ab751ea3861a07ea52e60ce6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aab54a98bcd2f7b77c3",
+    "url": "/static/js/2.c3bbc615.chunk.js"
+  },
+  {
+    "revision": "360978b69b2d355b7f6ed150ede4e119",
+    "url": "/static/js/2.c3bbc615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70438126e94b0d74c991",
+    "url": "/static/js/main.114d89e7.chunk.js"
+  },
+  {
+    "revision": "a60be9b8ca767962eb9f",
+    "url": "/static/js/runtime-main.5004d128.js"
+  }
+]);
